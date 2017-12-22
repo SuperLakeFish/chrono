141f79ac29855d39ba7ef129f8432a66f90fc6d9
Mike Taylor <mtaylor22@wisc.edu>
2017-12-21 22:38:38 -0600

    First cut at an updated set of more reasonable mass properties for the M113 model (Estimated based on part geometries).

--- src/chrono_models/vehicle/m113/M113_SprocketBandANCF.cpp
+++ src/chrono_models/vehicle/m113/M113_SprocketBandANCF.cpp
@@ -31,9 +31,9 @@ namespace m113 {
 // -----------------------------------------------------------------------------
 // Static variables
 // -----------------------------------------------------------------------------
-const double M113_SprocketBandANCF::m_gear_mass = 65.422;
-const ChVector<> M113_SprocketBandANCF::m_gear_inertia(1.053, 1.498, 1.053);
-const double M113_SprocketBandANCF::m_axle_inertia = 1;
+const double M113_SprocketBandANCF::m_gear_mass = 27.68;
+const ChVector<> M113_SprocketBandANCF::m_gear_inertia(0.646, 0.883, 0.646);
+const double M113_SprocketBandANCF::m_axle_inertia = 0.02;
 const double M113_SprocketBandANCF::m_separation = 0.278;
 
 // Gear profile data
--- src/chrono_models/vehicle/m113/M113_SprocketBandBushing.cpp
+++ src/chrono_models/vehicle/m113/M113_SprocketBandBushing.cpp
@@ -31,9 +31,9 @@ namespace m113 {
 // -----------------------------------------------------------------------------
 // Static variables
 // -----------------------------------------------------------------------------
-const double M113_SprocketBandBushing::m_gear_mass = 436.7;
-const ChVector<> M113_SprocketBandBushing::m_gear_inertia(12.22, 13.87, 12.22);
-const double M113_SprocketBandBushing::m_axle_inertia = 1;
+const double M113_SprocketBandBushing::m_gear_mass = 27.68;
+const ChVector<> M113_SprocketBandBushing::m_gear_inertia(0.646, 0.883, 0.646);
+const double M113_SprocketBandBushing::m_axle_inertia = 0.02;
 const double M113_SprocketBandBushing::m_separation = 0.278;
 
 // Gear profile data
